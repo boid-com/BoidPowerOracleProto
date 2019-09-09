f27c29efefa4badba3b266087de0fd4a0a50002d
john-boid <john@boid.com>
2019-09-09 06:21:31 -0500

    progress

--- server/util/handleDevicePowerReport.js
+++ server/util/handleDevicePowerReport.js
@@ -1,11 +1,22 @@
 
 const ecc = require('eosjs-ecc')
+const db = require('../../db')
 const validatorKeys = ['EOS7amPpRPLe3F2k6D3ctiS9uoa3XpeB4ePPYXtY7bsqkzDuV4tF8','EOS5gvHQg4ZS8NtCvHDBURm7db1UZKSzL5Wy46H1nWZVjaKiSndAE']
 ecc.initialize()
 
 async function injestPowerReport(data,pubKey){
-  // do something important here //
-  console.log(data,pubKey)
+  try {
+    for (report of data.powerReports){
+      db.gql(``)
+    }
+
+    // await db.gql(``)
+    // do something important here //
+    console.log(data,pubKey)
+    return {}
+  } catch (error) {
+    return {error}
+  }
 }
 
 
@@ -16,6 +27,7 @@ async function init(data,auth){
     if (!validatorKeys.find(el => el == pubKey)) return {error:'Authentication Failed',code:401}
     console.log('Data Signature Validated')
     const result = await injestPowerReport(data,pubKey)
+    return result
   } catch (error) {
     console.log(error)
     return {error:"Internal server error",code:500}
